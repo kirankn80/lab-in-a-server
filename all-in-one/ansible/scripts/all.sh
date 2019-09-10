5ad5687b1621eb185261fbf2958980b01aaafc91
Aprathi K <aprathik@nodem1.englab.juniper.net>
2019-09-10 15:37:25 +0530

    change in docker-ce version

--- all-in-one/ansible/scripts/all.sh
+++ all-in-one/ansible/scripts/all.sh
@@ -1,13 +1,22 @@
 #!/bin/bash -ex
 echo "Configure contrail deployment"
 cd /root/contrail-ansible-deployer
+echo $PWD
+ansible --version
+env
 ansible-playbook -i inventory/ -e orchestrator=openstack playbooks/configure_instances.yml
 
 echo "Install Openstack"
 cd /root/contrail-ansible-deployer
+echo $PWD
+ansible --version
+env
 ansible-playbook -i inventory/ playbooks/install_openstack.yml
 
 echo "Install contrail"
 cd /root/contrail-ansible-deployer
+echo $PWD
+ansible --version
+env
 ansible-playbook -i inventory/ -e orchestrator=openstack playbooks/install_contrail.yml
 

2cd2405c3b6a7dbc3845bc65601579dc307b8339
Aprathi K <aprathik@nodem1.englab.juniper.net>
2019-09-25 09:51:46 +0530

    new files

---
+++ all-in-one/ansible/scripts/scripts/docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -ex
+`pip uninstall -y docker` || true 
+`pip uninstall -y docker-py` || true
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce-18.03.1.ce
+#yum install -y docker-ce
+systemctl enable docker
+systemctl start docker
+echo '{"insecure-registries": ["nodei40.englab.juniper.net:5000", "nodei40.englab.juniper.net:5010", "ci-repo.englab.juniper.net:5010"]}' >> /etc/docker/daemon.json
+systemctl restart docker
+

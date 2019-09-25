b97b712d5f77d08fd39b7c824a9544273e8e0781
Aprathi K <aprathik@nodem1.englab.juniper.net>
2019-09-25 10:57:14 +0530

    additional roles

--- all-in-one/ansible/roles/roles/devenv/files/scons_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /root/contrail-dev-env
-make setup
-export KERNEL_VER=3.10.0-693.el7.x86_64
-export KERNELDIR=/lib/modules/$KERNEL_VER/build
-cd /root/contrail
-scons --kernel-dir=$KERNELDIR

6623691f5f48f76c63bd3e7f6a213e078119c185
aprathi <aprathik@juniper.net>
2020-10-23 15:39:28 +0530

    1. Adding docker login for hub registry - cc provisioning fails from hub 2. Changing base_pkgs to include only setting up ssh packages. Only contrail provisioner node needs these packages. Impact - When contrail version is not given in input yaml file, no packages are installed on empty vm.

--- ansible/scripts/cc.sh
+++ ansible/scripts/cc.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -ex
 systemctl disable firewalld
 systemctl stop firewalld
+HUB="hub.juniper.net/contrail-nightly"
+if [[ "$CCD_IMAGE" = *"$HUB"* ]]; then
+  docker login hub.juniper.net --username "JNPR-CSRXFieldUser12" --password "d2VbRJ8xPhSUAwzo7Lym"
+fi
 docker run -t --net host -e orchestrator=openstack -e action=import_cluster -v $COMMAND_SERVERS_FILE:/command_servers.yml -v $INSTANCES_FILE:/instances.yml -d --privileged --name contrail_command_deployer $CCD_IMAGE
 docker logs -f contrail_command_deployer
